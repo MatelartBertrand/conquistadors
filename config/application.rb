110e2688138e647cef0810a5493c03907696e2e1
Bertrand Matelart <matelart.bertrand@gmail.com>
2017-09-07 20:01:18 +0200

    debut homepage

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module Conquistadors
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
